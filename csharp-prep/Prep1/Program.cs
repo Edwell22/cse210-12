4755b5a377a314b3ae734e6ec7945bec869a1c2c
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2023-04-23 05:14:04 +0200

    new

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the user for their name.
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last} ,{first}, {last}.");
     }
 }
