c7ab3c5d9fb5f8ce475eb3d1294677321ebc9a2c
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2023-04-30 08:55:43 +0200

    new

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+    
+
+{
+    {
+    
+        {
+            List<int> numberList = new List<int>();
+
+            Console.WriteLine("Enter a series of numbers, one at a time. Enter 0 to stop.");
+
+            int input = int.Parse(Console.ReadLine());
+            while (input != 0)
+            {
+                numberList.Add(input);
+                input = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("The numbers you entered are:");
+            foreach (int number in numberList)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+
     }
 }
