cad8af65ca371aeb92a1c3f2af3e6907dd3f829a
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2023-04-30 09:39:00 +0200

    new p

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,57 @@
 using System;
 
-class Program
+namespace SimpleFunctions
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main(string[] args)
+        {
+            DisplayWelcomeMessage();
+
+            string name = PromptForName();
+
+            int number = PromptForNumber();
+
+            int squaredNumber = SquareNumber(number);
+
+            DisplayResult(name, number, squaredNumber);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Simple Functions program!");
+        }
+
+        static string PromptForName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptForNumber()
+        {
+            Console.Write("Please enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int squaredNumber = number * number;
+            return squaredNumber;
+        }
+
+        static void DisplayResult(string name, int number, int squaredNumber)
+        {
+            Console.WriteLine("Hello, " + name + "!");
+            Console.WriteLine("The number you entered was " + number + ".");
+            Console.WriteLine("The square of " + number + " is " + squaredNumber + ".");
+
+        }
     }
 }
+    
