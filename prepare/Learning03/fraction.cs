c8db9ba9690a26fff5df9c2d1a1f7e8244af5458
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2023-05-21 06:16:56 +0200

    new

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,41 @@
+using System;
+//fraction code
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        // Default to 1/1
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+    
+        // Is is just a temporary, local variable that will be recomputed each time this is called.
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        
+        // Is will be recomputed each time this is called.
+        return (double)_top / (double)_bottom;
+    }
+}
