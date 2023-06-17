14fe2c5c9781d4827dbbccb9c023ec9a3047dbfa
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2023-06-17 15:00:46 +0200

    new

---
+++ prepare/Learning05/circle.cs
@@ -0,0 +1,9 @@
+public Circle(string color, double radius) : base(color)
+{
+    this.radius = radius;    
+}  
+    
+public override double GetArea() 
+{
+    return Math.PI * radius * radius;   
+}
