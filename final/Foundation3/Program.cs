935aaa7d783e6b68e10fca2dba1782ec521e9662
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2023-07-16 06:28:46 +0200

    news

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,167 @@
 using System;
 
+class Address
+{
+    private readonly string street;
+    private readonly string city;
+    private readonly string state;
+    private readonly string country;
+
+    public string Street => street;
+    public string City => city;
+    public string State => state;
+    public string Country => country;
+
+    public Address(string street, string city, string state, string country)
+    {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    public override string ToString()
+    {
+        return $"{street}, {city}, {state}, {country}";
+    }
+}
+
+class Event
+{
+    private readonly string title;
+    private readonly string description;
+    private readonly DateTime date;
+    private readonly TimeSpan time;
+    private readonly Address address;
+
+    public string Title => title;
+    public string Description => description;
+    public DateTime Date => date;
+    public TimeSpan Time => time;
+    public Address Address => address;
+
+    public Event(string title, string description, DateTime date, TimeSpan time, Address address)
+    {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.address = address;
+    }
+
+    public virtual string GetStandardDetails()
+    {
+        return $"Title: {title}\nDescription: {description}\nDate: {date.ToShortDateString()}\nTime: {time}\nAddress: {address}";
+    }
+
+    public virtual string GetFullDetails()
+    {
+        return GetStandardDetails();
+    }
+
+    public virtual string GetShortDescription()
+    {
+        return $"Type: {GetType().Name}\nTitle: {title}\nDate: {date.ToShortDateString()}";
+    }
+}
+
+class Lecture : Event
+{
+    private readonly string speaker;
+    private readonly int capacity;
+
+    public string Speaker => speaker;
+    public int Capacity => capacity;
+
+    public Lecture(string title, string description, DateTime date, TimeSpan time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        this.speaker = speaker;
+        this.capacity = capacity;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nType: Lecture\nSpeaker: {speaker}\nCapacity: {capacity}";
+    }
+}
+
+class Reception : Event
+{
+    private readonly string rsvpEmail;
+
+    public string RsvpEmail => rsvpEmail;
+
+    public Reception(string title, string description, DateTime date, TimeSpan time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        this.rsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nType: Reception\nRSVP Email: {rsvpEmail}";
+    }
+}
+
+class OutdoorGathering : Event
+{
+    private readonly string weather;
+
+    public string Weather => weather;
+
+    public OutdoorGathering(string title, string description, DateTime date, TimeSpan time, Address address, string weather)
+        : base(title, description, date, time, address)
+    {
+        this.weather = weather;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{base.GetFullDetails()}\nType: Outdoor Gathering\nWeather: {weather}";
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        var address1 = new Address("123 Main St", "Anytown", "CA", "USA");
+        var lecture1 = new Lecture("Lecture 1", "Description of Lecture 1", new DateTime(2023, 7, 16), new TimeSpan(14, 0, 0), address1, "John Smith", 50);
+
+        var address2 = new Address("456 High St", "Anycity", "ON", "Canada");
+        var reception1 = new Reception("Reception 1", "Description of Reception 1", new DateTime(2023, 7, 17), new TimeSpan(18, 0, 0), address2, "rsvp@example.com");
+
+        var address3 = new Address("789 Park Ave", "Anyville", "NY", "USA");
+        var outdoorGathering1 = new OutdoorGathering("Outdoor Gathering 1", "Description of Outdoor Gathering 1", new DateTime(2023, 7, 18), new TimeSpan(12, 0, 0), address3, "Sunny and warm");
+
+        Console.WriteLine("Lecture 1");
+        Console.WriteLine("Standard Details:");
+        Console.WriteLine(lecture1.GetStandardDetails());
+        Console.WriteLine("Full Details:");
+        Console.WriteLine(lecture1.GetFullDetails());
+        Console.WriteLine("Short Description:");
+        Console.WriteLine(lecture1.GetShortDescription());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Reception 1");
+        Console.WriteLine("Standard Details:");
+       
+        Console.WriteLine(reception1.GetStandardDetails());
+        Console.WriteLine("Full Details:");
+        Console.WriteLine(reception1.GetFullDetails());
+        Console.WriteLine("Short Description:");
+        Console.WriteLine(reception1.GetShortDescription());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Outdoor Gathering 1");
+        Console.WriteLine("Standard Details:");
+        Console.WriteLine(outdoorGathering1.GetStandardDetails());
+        Console.WriteLine("Full Details:");
+        Console.WriteLine(outdoorGathering1.GetFullDetails());
+        Console.WriteLine("Short Description:");
+        Console.WriteLine(outdoorGathering1.GetShortDescription());
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,201 @@
 using System;
+using System.Collections.Generic;
+
+abstract class Activity
+{
+    private readonly DateTime date;
+    private readonly int length;
+
+    public DateTime Date => date;
+    public int Length => length;
+
+    public Activity(DateTime date, int length)
+    {
+        this.date = date;
+        this.length = length;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary(bool useKm = true)
+    {
+        string unit = useKm ? "km" : "miles";
+        string speedUnit = useKm ? "kph" : "mph";
+        string paceUnit = useKm ? "min per km" : "min per mile";
+
+        return $"{date.ToShortDateString()} {GetType().Name} ({length} min) - Distance: {GetDistance(useKm):F2} {unit}, Speed: {GetSpeed(useKm):F1} {speedUnit}, Pace: {GetPace(useKm):F1} {paceUnit}";
+    }
+}
+
+class Running : Activity
+{
+    private readonly double distance;
+
+    public double Distance => distance;
+
+    public Running(DateTime date, int length, double distance)
+        : base(date, length)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return distance / (Length / 60.0);
+    }
+
+    public override double GetPace()
+    {
+        return Length / distance;
+    }
+}
+
+class Cycling : Activity
+{
+    private readonly double speed;
+
+    public double Speed => speed;
+
+    public Cycling(DateTime date, int length, double speed)
+        : base(date, length)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return speed * (Length / 60.0);
+    }
+
+    public override double GetSpeed()
+    {
+        return speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60.0 / speed;
+    }
+}
+
+class Swimming : Activity
+{
+    private readonly int laps;
+
+    public int Laps => laps;
+
+    public Swimming(DateTime date, int length, int laps)
+        : base(date, length)
+    {
+        this.laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return laps * 50.0 / 1000.0;
+    }
+
+    public override double GetSpeed()
+    {
+        return GetDistance() / (Length / 60.0);
+    }
+
+    public override double GetPace()
+    {
+        return Length / GetDistance();
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        // Prompt the user to choose between kilometers and miles
+        Console.WriteLine("Choose distance unit:");
+        Console.WriteLine("1. Kilometers");
+        Console.WriteLine("2. Miles");
+        string unitInput = Console.ReadLine();
+        bool useKm = unitInput == "1";
+
+        var activities = new List<Activity>();
+
+        while (true)
+        {
+            // Prompt the user to choose an activity type
+            Console.WriteLine("Choose an activity type:");
+            Console.WriteLine("1. Running");
+            Console.WriteLine("2. Cycling");
+            Console.WriteLine("3. Swimming");
+            Console.WriteLine("4. Quit");
+
+            string input = Console.ReadLine();
+            if (input == "4")
+            {
+                break;
+            }
+
+            // Prompt the user for the date and length of the activity
+            Console.WriteLine("Enter the date (dd/mm/yyyy):");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the length (in minutes):");
+            int length = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case "1":
+                    // Prompt the user for the distance of the run
+                    Console.WriteLine("Enter the distance (in kilometers):");
+                    double distance;
+                    if (!double.TryParse(Console.ReadLine(), out distance))
+                    {
+                        Console.WriteLine("Invalid input for distance, please try again.");
+                        continue;
+                    }
+                    activities.Add(new Running(date, length, distance));
+                    break;
+
+                case "2":
+                    // Prompt the user for the speed of the bike ride
+                    Console.WriteLine("Enter the speed (in kilometers per hour):");
+                    double speed;
+                    if (!double.TryParse(Console.ReadLine(), out speed))
+                    {
+                        Console.WriteLine("Invalid input for speed, please try again.");
+                       continue;
+                    }
+                    activities.Add(new Cycling(date, length, speed));
+                    break;
+
+                case"3":
+                    // Prompt the user for the number of laps swam
+                    Console.WriteLine("Enter the number of laps:");
+                    int laps;
+                    if (!int.TryParse(Console.ReadLine(), out laps))
+                    {
+                        Console.WriteLine("Invalid input for number of laps, please try again.");
+                        continue;
+                    }
+                    activities.Add(new Swimming(date, length, laps));
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input, please try again.");
+                    break;
+            }
+        }
+
+        // Display the summary information for each activity
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary(useKm));
+        }
     }
 }
